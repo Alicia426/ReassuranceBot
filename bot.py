25e52636e72689ecb1a45a9f257d67948104d446
Alicia426 <you@example.com>
2021-09-03 22:01:51 -0500

    db works, reassurance works, I have a headache. The stuffie thing is hard to figure out

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from advice import advice_list
 from tryout import tryout_string
 from reassurance import reassurances
 
+os.chdir('appdata')
 client = discord.Client()
 
 commands = ('!help', '!configure', '!hello', '!reassure',
@@ -24,6 +25,7 @@ def pronounParser(pronoun_string):
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    print("My working directory is", os.getcwd())
     print("I know", len(advice_list), "pieces of advice.")
     print("I have", len(reassurances), "reassurance affimations")
 
@@ -43,10 +45,18 @@ async def on_message(message):
     # Configures user name and pronouns
     if message.content.startswith(commands[1]):
         print(message.author, message.content)
+        uid=str(message.author)
         name = message.content.split(" ")[1]
         pronouns = pronounParser(message.content.split(" ")[2])
-        print(name, pronouns)
-        await message.channel.send('WIP 1')
+        print(uid, name, pronouns)
+        try:
+            ymldb.createUser(uid,name, pronouns)
+            check=ymldb.fetchUserData(uid)
+            msg= check[0]+", I will remember you."
+            await message.channel.send(msg)
+        except Exception as e:
+            print(e)
+        
 
     # Says hello
     if message.content.startswith(commands[2]):
@@ -55,8 +65,13 @@ async def on_message(message):
 
     # Reassures user or stufie
     if message.content.startswith(commands[3]):
-        print(message.author, message.content)
-        await message.channel.send('WIP 3')
+        uid=str(message.author)
+        #cmd=message.content
+        user_data=ymldb.fetchUserData(uid)
+        print(user_data)
+        name=user_data[0]
+        msg=random.choice(reassurances).format(name=name)
+        await message.channel.send(msg)
 
     # Gives life advice
     if message.content.startswith(commands[4]):
@@ -64,7 +79,7 @@ async def on_message(message):
         adv = random.choice(advice_list)
         await message.channel.send(adv)
 
-    # Gives life advice
+    # Configures_stuffie
     if message.content.startswith(commands[5]):
         print(message.author, message.content)
         await message.channel.send('WIP 5')
@@ -82,20 +97,24 @@ async def on_message(message):
         name = message.content.split(" ")[1]
         pronouns = pronounParser(message.content.split(" ")[2])
         print(name, pronouns)
-        if pronouns["sps"] == "they":
-            s = ""
-            spob = "their"
-        else:
-            spob = pronouns["spo"]
-            s = "s"
-        msg = tryout_string.format(spob=spob,
-                                   name=name,
-                                   spo=pronouns["spo"],
-                                   sps=pronouns["sps"],
-                                   sp=pronouns["sp"],
-                                   sr=pronouns["sr"],
-                                   s=s)
-        await message.channel.send(msg)
+        try:
+            if pronouns["sps"] == "they":
+                s = ""
+                spob = "their"
+            else:
+                spob = pronouns["spo"]
+                s = "s"
+            msg = tryout_string.format(spob=spob,
+                                    name=name,
+                                    spo=pronouns["spo"],
+                                    sps=pronouns["sps"],
+                                    sp=pronouns["sp"],
+                                    sr=pronouns["sr"],
+                                    s=s)
+            await message.channel.send(msg)
+        except Exception as e:
+            await message.channel.send(e)
+
 
 
 while True:
--- ymldb.py
+++ ymldb.py
@@ -2,24 +2,56 @@ import yaml
 import glob
 import os
 
-def ensureDir():
-    if os.getcwd().split("/")[-1] != 'user_data':
-        os.chdir('user_data')
 
 def fetchUser(id):
-    ensureDir()
+    print(id)
     for file in glob.glob("*.yml"):
-        if file.replace(".yml",'') == id:
-            #print(file, 'found')
+        print(file,file.replace(".yml", ''))
+        if file.replace(".yml", '') == id:
+            print(file, 'found')
             return file
+
     return None
 
-def createUser(id,name,pronoun_dict):
-    ensureDir()
 
-def createStuffie(id,stuffie_name, stuffe_pronun_dict):
-    ensureDir()
+def fetchUserData(id):
+    fn = fetchUser(id)
+    print(fn)
+    if fn != None:
+        with open(fn) as f:
+            doc = yaml.load(f, Loader=yaml.FullLoader)
+            name = doc["name"]
+            pronouns = doc["pronouns"]
+            stuffie = doc["stuffie"]
+
+            return (name, pronouns, stuffie)
+
+    else:
+        return "User not found, must be configured first"
+    
 
 
-print(fetchUser("Alicia#9680"))
-print(fetchUser("K"))
+def createUser(id, name, pronoun_dict):
+    fn = id+".yml"
+    user_dict = {
+        "name": name,
+        "pronouns": pronoun_dict,
+        "stuffie":
+        {
+            'name': 'null',
+            'pronouns':
+                    {'sps': 'null', 'spo': 'null', 'sp': 'null', 'sr': 'null'}}
+    }
+    with open(fn, 'w') as f:
+        yaml.dump(user_dict, f)
+
+
+def createStuffie(id, stuffie_name, stuffe_pronun_dict):
+    pass
+
+
+# print(fetchUser("Alicia#9680"))
+# print(fetchUser("K"))
+#print(fetchUserData("Alicia#9680"))
+#createUser("test#123", "TestGal", {
+#           'sps': 'she', 'spo': 'her', 'sp': 'hers', 'sr': 'herself'})
